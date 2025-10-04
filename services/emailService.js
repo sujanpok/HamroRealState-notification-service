24371225346aa36489998885f26e8d24922f6982
Sujan Pokharel <you@example.com>
2025-10-05 00:43:54 +0900

    error fixed

--- services/emailService.js
+++ services/emailService.js
@@ -1,10 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
+import 'dotenv/config';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 console.log('Resend initialized with API key:', process.env.RESEND_API_KEY);
+
 export async function sendEmail({ from, to, subject, html }) {
   const fullFrom = `${from}@${process.env.EMAIL_DOMAIN}`;
   console.log(`Sending email from: ${fullFrom} to: ${to} with subject: ${subject}`);
